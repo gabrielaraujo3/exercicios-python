37a431854205e489cb7cede8fcae459bfef75a39
Gabriel <araujog388@gmail.com>
2021-09-19 17:47:54 -0300

    ultimos exercicios cursoev

---
+++ exercicios/ex108/teste.py
@@ -0,0 +1,12 @@
+from ex108 import moeda
+
+'''p = float(input('Digite o preço: R$'))
+print(f'A metade de R${p} é R${moeda.metade(p)}')
+print(f'O dobro de R${p} é R${moeda.dobro(p)}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}')'''
+
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'O Dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(p,10))}')
